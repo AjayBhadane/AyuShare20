e2918d2655a7bfe758d0f8b0de379cb97206550d
AjayBhadane <ajaybhadane95@gmail.com>
2018-12-19 19:22:19 +0530

    First commit

--- app/src/main/java/com/devices/ayu/ayushare20/MainActivity.java
+++ app/src/main/java/com/devices/ayu/ayushare20/MainActivity.java
@@ -1,34 +1,26 @@
 package com.devices.ayu.ayushare20;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    private void hideTitle(){
-
-    }
+    private static final int FULLSCREEN = WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullscreen();
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void setFullscreen(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(FULLSCREEN, FULLSCREEN);
+
+        getSupportActionBar().hide();
+    }
 
 }
